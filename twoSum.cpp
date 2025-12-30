3bafbd47a35055fc984e5adcaa2025564d827762
Jojicoder <joji.kashimura@live.lagcc.cuny.edu>
2025-12-30 00:14:47 -0500

    Map

--- twoSum.cpp
+++ twoSum.cpp
@@ -9,11 +9,11 @@ class Solution{
         vector<int> twoSum(vector<int>& nums, int target){
             unordered_map<int, int> map;
 
-            for(int i = 0; i<nums.size();i++){
+            for(int i = 0;i < nums.size();i++){
                 int complement = target - nums[i];
 
                 if(map.count(complement)){
-                    return{map[complement], i};
+                    return {map[complement], i};
                 }
                 map[nums[i]] = i;
             }
