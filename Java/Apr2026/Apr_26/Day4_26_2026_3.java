c46b00be5f972846f0e6bd50c84bbf600140de60
Jojicoder <joji.kashimura@live.lagcc.cuny.edu>
2026-04-29 23:39:06 -0400

    4/29/2029

---
+++ Java/Apr2026/Apr_26/Day4_26_2026_3.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Day4_26_2026_3 {
+    public static void main(String[] args){
+        ArrayList<String> names = new ArrayList<>();
+        names.add("alice");
+        names.add("bob");
+        names.add("charlie");
+
+        name.stream()
+            .map(n -> n.toUpper)
+                            
+    }
+}
