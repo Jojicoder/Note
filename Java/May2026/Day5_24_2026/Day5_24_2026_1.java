359bed37ac4e383e905bc5bb5258f49d34d0c7cc
Jojicoder <joji.kashimura@live.lagcc.cuny.edu>
2026-05-27 22:57:32 -0400

    5/27/2026

---
+++ Java/May2026/Day5_24_2026/Day5_24_2026_1.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+public class Day5_24_2026_1 {
+    public static boolean isAnagram(String s, String t){
+        if(s.length() != t.length()) return false;
+
+        HashMap<Character,Integer> map = new HashMap<>();
+
+        for(char c : s.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+
+        for(char c : t.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)-1);
+        }
+
+        for(int count : map.values()){
+            if(count != 0) return false;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args){
+        System.out.println(isAnagram("anagram","nagaram"));
+        System.out.println(isAnagram("rat","car"));
+
+    }
+}
---
+++ Java/May2026/Day5_25_2026/Day5_25_2026.java
@@ -0,0 +1,19 @@
+public class Day5_25_2026 {
+    
+    public static int maxSubArray(int[] nums){
+     int currentSum = nums[0];
+     int maxSum = nums[0];
+
+     for(int i = 1;i<nums.length;i++){
+        currentSum = Math.max(currentSum+nums[i],nums[i]);
+        maxSum = Math.max(currentSum,maxSum);
+     }
+     return maxSum;
+    }
+
+    public static void main(String[] args){
+        System.out.println(maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4})); // 6
+        System.out.println(maxSubArray(new int[]{1}));                               // 1
+        System.out.println(maxSubArray(new int[]{5, 4, -1, 7, 8})); 
+    }
+}
